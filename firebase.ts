249bccae410bd9d898c071418cdce74055de51be
Dan Foley <32817606+d-otis@users.noreply.github.com>
2024-08-03 21:53:29 -0500

    connect to rtdb emulator when localhost

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import { DATABASE_URL } from "./databaseUrl";
 
 const {
@@ -33,3 +33,8 @@ export const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
+
+if (typeof location !== "undefined" && location.hostname === "localhost") {
+  // Point to the RTDB emulator running on localhost.
+  connectDatabaseEmulator(database, "127.0.0.1", 9000);
+}
