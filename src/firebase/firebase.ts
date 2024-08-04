2b4523319d429e2542973292100b79ffa65128a9
Dan Foley <32817606+d-otis@users.noreply.github.com>
2024-08-04 14:20:49 -0500

    remove unneeded import

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import * as config from "./firebaseConfig";
 import { DATABASE_URL } from "../../databaseUrl";
 
